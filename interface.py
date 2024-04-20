01dfd45dde05c257582d7877a68d84cf0a20fccd
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2024-04-20 20:10:45 +0200

    Init tabs

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ class Interface(tk.Tk):
         super().__init__()
         self.title("White List")
         self.iconbitmap(r"tax.ico")
-        self.geometry("300x200")
+        self.geometry("600x450")
 
         # Styles
         self.ttk_style = ttk.Style()
@@ -35,17 +35,25 @@ class Interface(tk.Tk):
         self.photo_label.pack(fill="both", expand=True)
         self.photo_label.bind("<Configure>", resize_image)
 
+        # Notebooks
+        self.notebook = ttk.Notebook(self.frame1)
+        self.tab1 = ttk.Frame(self.notebook)
+        self.tab2 = ttk.Frame(self.notebook)
+        self.notebook.add(self.tab1, text="Pojedyncza weryfikacja")
+        self.notebook.add(self.tab2, text="Weryfikacja listy w Excelu")
+        self.notebook.pack(fill='both', expand=True, padx=10, pady=10)
+
         # Check buttons
         self.check_var_1 = tk.BooleanVar(value=True)
-        self.check_1 = ttk.Checkbutton(self.frame1, text="Check 1", variable=self.check_var_1)
+        self.check_1 = ttk.Checkbutton(self.tab1, text="Check 1", variable=self.check_var_1)
         self.check_1.pack(anchor='w')
 
         self.check_var_2 = tk.BooleanVar(value=False)
-        self.check_2 = ttk.Checkbutton(self.frame1, text="Check 2", variable=self.check_var_2)
+        self.check_2 = ttk.Checkbutton(self.tab1, text="Check 2", variable=self.check_var_2)
         self.check_2.pack(anchor='w')
 
         self.check_var_3 = tk.BooleanVar(value=True)
-        self.check_3 = ttk.Checkbutton(self.frame1, text="Check 3", variable=self.check_var_3)
+        self.check_3 = ttk.Checkbutton(self.tab1, text="Check 3", variable=self.check_var_3)
         self.check_3.pack(anchor='w')
 
         # Run button
