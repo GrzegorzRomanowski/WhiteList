9e964942e2c8022d07b7157c91f8e380ca9df04d
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2024-04-20 22:00:51 +0200

    During tab1 modifications part 1

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+from browser import WhiteListBrowser
+
 
 class Interface(tk.Tk):
     def __init__(self):
@@ -39,32 +41,45 @@ class Interface(tk.Tk):
         self.notebook = ttk.Notebook(self.frame1)
         self.tab1 = ttk.Frame(self.notebook)
         self.tab2 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab1, text="Pojedyncza weryfikacja")
-        self.notebook.add(self.tab2, text="Weryfikacja listy w Excelu")
+        self.notebook.add(self.tab1, text=" Pojedyncza weryfikacja   ")
+        self.notebook.add(self.tab2, text=" Weryfikacja listy w Excelu   ")
         self.notebook.pack(fill='both', expand=True, padx=10, pady=10)
 
-        # Check buttons
-        self.check_var_1 = tk.BooleanVar(value=True)
-        self.check_1 = ttk.Checkbutton(self.tab1, text="Check 1", variable=self.check_var_1)
-        self.check_1.pack(anchor='w')
+        # Radio buttons
+        self.validation_method_var = tk.IntVar(value=1)
+        self.method_1 = ttk.Radiobutton(self.tab1, text="Numer konta", variable=self.validation_method_var, value=1)
+        self.method_1.pack(anchor='nw')
+        self.method_2 = ttk.Radiobutton(self.tab1, text="NIP", variable=self.validation_method_var, value=2)
+        self.method_2.pack(anchor='nw')
+        self.method_3 = ttk.Radiobutton(self.tab1, text="REGON", variable=self.validation_method_var, value=3)
+        self.method_3.pack(anchor='nw')
 
-        self.check_var_2 = tk.BooleanVar(value=False)
-        self.check_2 = ttk.Checkbutton(self.tab1, text="Check 2", variable=self.check_var_2)
-        self.check_2.pack(anchor='w')
+        self.changed_date_var = tk.BooleanVar(value=False)
+        self.changed_date = ttk.Checkbutton(self.tab1, text="Inna data niż dzisiejsza?", variable=self.changed_date_var, command=self.entry_on_off)
+        self.changed_date.pack(anchor='w')
+
+        self.entry_date = ttk.Entry(self.tab1, state="disabled")
+        self.entry_date.pack(anchor='w')
 
         self.check_var_3 = tk.BooleanVar(value=True)
         self.check_3 = ttk.Checkbutton(self.tab1, text="Check 3", variable=self.check_var_3)
-        self.check_3.pack(anchor='w')
+        self.check_3.pack(anchor='sw')
 
         # Run button
-        self.run_button = ttk.Button(self.frame2, text="RUN", command=self.get_check_buttons)
+        self.run_button = ttk.Button(self.tab1, text="RUN", command=self.get_check_buttons)
         self.run_button.pack()
 
         self.mainloop()
 
+    def entry_on_off(self):
+        if self.changed_date_var.get():
+            self.entry_date.config(state="normal")
+        else:
+            self.entry_date.config(state="disabled")
+
     def get_check_buttons(self):
-        print(self.check_var_1.get(), self.check_var_2.get(), self.check_var_3.get())
-        return self.check_var_1.get(), self.check_var_2.get(), self.check_var_3.get()
+        print(self.validation_method_var.get(), self.changed_date_var.get(), self.check_var_3.get())
+        return self.validation_method_var.get(), self.changed_date_var.get(), self.check_var_3.get()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from browser import WhiteListBrowser
-from browser import white_list_url
 from interface import Interface
 
 
 if __name__ == "__main__":
-    white_list_obj = WhiteListBrowser(url=white_list_url)
-    white_list_obj()
-
     gui_obj = Interface()
