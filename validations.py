db0a9d7a313a1e5477cf18e9e716f3a1eb7d068f
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2024-04-21 11:40:22 +0200

    bank accounts validation

--- validations.py
+++ validations.py
@@ -30,9 +30,28 @@ def is_valid_nip(nip: str) -> bool:
 
 def is_bank_account_valid(bank: str) -> bool:
     bank = cleaning(bank)
+    bank = bank[-24:] + "2521" + bank[-26:-24]
+
+    # Is numeric and have 30 digits?
+    if len(bank) != 30 or (not bank.isnumeric()):
+        return False
+
+    # modulo
+    if int(bank) % 97 == 1:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
     # Shouldn't be launched directly - only for debugging purposes
-    print("NIP validation:         ", is_valid_nip("593-21-67-267"))
-    print("Bank account validation:", is_bank_account_valid("77 1240 1268 1111 0010 6850 0503"))
+    assert is_valid_nip("593-21-67-267")
+    assert not is_valid_nip("593-21-67-268")  # "Wrong modulo"
+    assert not is_valid_nip("593-21-67-26")  # "Too short"
+    assert not is_valid_nip("593-21-6xxx7-267")  # "Containing letters"
+
+    assert is_bank_account_valid("77 1240 1268 1111 0010 6850 0503")
+    assert is_bank_account_valid("PL77 1240 1268 1111 0010 6850 0503")
+    assert not is_bank_account_valid("PL77 1240 1268 1111 0010 6850 0504")  # Wrong modulo
+    assert not is_bank_account_valid("77 1240 1268 1111 0010 6850")  # "Too short"
+    assert not is_bank_account_valid("77 1240 1268 1111 text 0010 6850 0503")  # "Containing letters (not in prefix)"
