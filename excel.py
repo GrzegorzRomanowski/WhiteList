967368c1cfa27f74b94e050cc853b6ba7a535bce
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2024-04-21 16:15:39 +0200

    validate bank accounts and make list for browser

--- excel.py
+++ excel.py
@@ -6,18 +6,21 @@ from validations import only_digits, is_bank_account_valid
 
 class BulkData:
     def __init__(self):
-        self.input_df = pd.read_excel(r"data/input.xlsx")
+        self.df = pd.read_excel(r"data/input.xlsx")
 
-    def make_list_for_browser(self):
-        accounts_list = self.input_df["Numer konta"].to_list()
+    def make_list_for_browser(self) -> list:
+        accounts_list = self.df["Numer konta"].to_list()
         accounts_list = [only_digits(account) for account in accounts_list if is_bank_account_valid(account)]
+        self.df["Rezultat"] = self.df["Numer konta"].apply(
+            lambda x: "" if is_bank_account_valid(x) else "BŁĘDNY NUMER KONTA")
         return accounts_list
 
-    def write_errors_to_df(self):
+    def write_scraped_data_to_df(self, results: dict):
         pass
 
 
 if __name__ == "__main__":
+    # Shouldn't be launched directly - only for debugging purposes
     bulk_data_obj = BulkData()
-    print(bulk_data_obj.input_df)
     print(bulk_data_obj.make_list_for_browser())
+    print(bulk_data_obj.df)
